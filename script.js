33835dac3f3901d6ef6538439b58ab3fad1b9674
karan raj <rajkaran4711@gmail.com>
2021-07-29 21:34:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const tempload= ()=>{
+     let temp = document.getElementById('temp');
+     temp.innerHTML='&#xf2cb';
+     temp.style.color="#ffffff";
+    
+
+     setTimeout(()=>{
+          temp.innerHTML='&#xf2ca'; 
+          temp.style.color="#ffff4d"
+          
+     },1000)
+     setTimeout(()=>{
+          temp.innerHTML='&#xf2c9'; 
+          temp.style.color="#ffff00"
+     },2000)
+     setTimeout(()=>{
+          temp.innerHTML='&#xf2c8'; 
+          temp.style.color="#ff4d4d"
+     },3000)
+     setTimeout(()=>{
+          temp.innerHTML='&#xf2c7'; 
+          temp.style.color="#b30000"
+     },4000)
+}
+
+tempload()
+setInterval(tempload,5000)
